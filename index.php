33cbb06ce9f760699e19baf59d0a2a7200f15916
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-07 15:54:50 +0200

    add style

--- index.php
+++ index.php
@@ -48,12 +48,13 @@ $hotels = [
     <title>PHP Hotel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="" method="get">
         <div>
             <label for="parking">Parcheggio</label>
-            <select id="parking" value="Scegli">
+            <select id="parking">
                 <option name="" selected>Scegli</option>
                 <option name="">Con Parcheggio</option>
             </select>
