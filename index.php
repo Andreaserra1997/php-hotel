cd62a323767be7e2f1812fc2413797da17874077
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-06-07 15:11:28 +0200

    add li hotel

--- index.php
+++ index.php
@@ -49,8 +49,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
   </head>
   <body>
-    
+    <ul><?php
+		foreach ($hotels as $hotel) { ?>
+			<li>
+            <?= "{$hotel['name']} {$hotel['description']} " . ($hotel['parking'] ? 'Sì' : 'No') . " {$hotel['vote']} {$hotel['distance_to_center']}" ?>
+            </li><?php
+		} ?>
+	</ul>
   </body>
 </html>
